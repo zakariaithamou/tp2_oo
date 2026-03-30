0174c2be47f3ad995913866862a0dff880968a0a
Zakaria AIT HAMOU <z.aithamou1635@uca.ac.ma>
2026-03-30 12:10:30 +0100

    Add second main method for complex number operations
    Added a second main method to demonstrate complex number operations.

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    //exo1
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle(10, 5);
         Rectangle r2 = new Rectangle(7, 7);
@@ -6,4 +7,18 @@ public class Main {
         r1.afficherRectangle();
         r2.afficherRectangle();
     }
+    //exo2
+     public static void main(String[] args){
+        Complexes z = new Complexes();
+        z.Afficher();
+        Complexes z1 = new Complexes(3,2);
+        z1.Afficher();
+        Complexes z2 = new Complexes(3,-6);
+        z2.Afficher();
+        Complexes z3 = z1.Plus(z2);
+        z3.Afficher();
+        Complexes z4 = z2.Moins(z3);
+        z4.Afficher();
+    }
+    
 }
